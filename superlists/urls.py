390a8a7e9cb59351f6d7ac350c45577c6c1cd7bf
Alberto B <criptoholden@gmail.com>
2017-06-28 15:47:10 +0200

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -22,6 +22,10 @@ urlpatterns = [
         views.home_page,
         name='home'),
 
+    url(r'^lists/the-only-list-in-the-world/$',
+        views.view_list,
+        name='view_list'),
+
     url(r'^admin/',
         admin.site.urls),
 ]
